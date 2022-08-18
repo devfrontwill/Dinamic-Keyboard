20911503edcac4cb5510ddf27ea1b0b9f5531b17
Wiliam Roza <devfrontwill@gmail.com>
2022-08-17 21:18:42 -0300

    Criado uma função

---
+++ main.js
@@ -0,0 +1,16 @@
+function tocaSomPom () {
+     document.querySelector ('#som_tecla_pom').play();         
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < 9) {
+    listaDeTeclas[0].onclick = tocaSomPom;
+
+    contador = contador +1;
+
+    console.log(contador);
+
+}
